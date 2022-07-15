46d57832ca5eb05f9054e0464c468f44fea155cc
alepereiraciandt <alexandrepereira@ciandt.com>
2022-07-15 15:40:11 -0300

    fix

--- 0x03/midia_player/AudioPlayer.java
+++ 0x03/midia_player/AudioPlayer.java
@@ -2,7 +2,7 @@ public class AudioPlayer implements MediaPlayer{
     @Override
     public void reproduzir(TipoMedia tipoMedia, String file) {
         if (tipoMedia.equals(TipoMedia.VLC)) {
-            System.out.print(String.format("Reproduzindo VCL: %s", file));
+            System.out.print(String.format("Reproduzindo VLC: %s", file));
         } else {
             System.out.print(String.format("Reproduzindo MP3: %s", file));
         }
